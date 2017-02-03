88125c87b1aa42f3834ec61005fd220f716e28ee
Fabian Popa <fabpopa@users.noreply.github.com>
2017-02-03 15:38:33 +0100

    Update preload.js, complete font and stylesheet preload

--- js/preload.js
+++ js/preload.js
@@ -33,6 +33,7 @@ const preload = (() => {
         if (!v.readyState || !v.buffered.length) v.onerror();
       }, 10000);
     };
+
     v.autoplay = true;
     v.muted = true;
     v.preload = 'auto';
@@ -42,13 +43,14 @@ const preload = (() => {
       if (v.buffered.end(0) === v.duration) { clear(); if (doneCb) doneCb(); }
     };
     v.onsuspend = () => {
+      // straight from cache, establish validity
       clear();
-      let xhr = new XMLHttpRequest();
+      const xhr = new XMLHttpRequest();
       xhr.open('GET', url);
       xhr.responseType = 'blob';
       xhr.onreadystatechange = () => {
         if (xhr.readyState === xhr.HEADERS_RECEIVED) {
-          let status = xhr.status;
+          const status = xhr.status;
           xhr.abort();
           if (status >= 200 && status < 300) { if (doneCb) doneCb(); }
           else { if (errCb) errCb(); }
@@ -63,18 +65,90 @@ const preload = (() => {
     resetTimeout();
   };
 
-  const font = (url, onComplete, onError) => {
+  // src is of the form 'url(...)'
+  // ffd is the FontFaceDescriptors object in the CSS Font Loading spec
+  // with additional 'family', 'src', and '_original' properties
+  // ffd._original is the '@font-face {...}' string if FontFace not available
+  // provide optional fontGlyphs if latin chars and punctuation are not in font
+  const font = (ffd, doneCb, errCb, fontGlyphs) => {
+    const native = () => {
+      const f = new FontFace(ffd.family, ffd.src, ffd);
+      document.fonts.add(f);
+      f.load().then(doneCb).catch(errCb);
+    };
 
+    const simulated = () => {
+      if (!ffd._original) throw new Error('Fallback, ffd._original required.');
+      if (!fontGlyphs) fontGlyphs = 'x-1;,.@';
+      const s = document.createElement('style');
+      s.innerHTML = ffd._original;
+      document.head.appendChild(s);
+      const el = document.createElement('p');
+      const propRe = /font-.+?;/g;
+      let match;
+      while (match = propRe.exec(ffd._original)) el.style.cssText += match[0];
+      el.innerHTML = fontGlyphs;
+      p.appendChild(el);
+
+      // load font file redundantly to get an event after enough time
+      const clean = () => { document.head.removeChild(s); p.removeChild(el); };
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', /url\(['"]?(.+?)['"]?\)/.exec(ffd.src)[1]);
+      xhr.responseType = 'blob';
+      xhr.onload = () => { clean(); if (doneCb) doneCb(); };
+      xhr.onerror = () => { clean(); if (errCb) errCb(); }
+      xhr.send();
+    };
+
+    if (window.FontFace) { native(); } else { simulated(); }
   };
 
-  const fromStylesheet = () => {
+  // doneCb will be called with another fn to attach the style doneCb(attachCb)
+  // provide optional fontGlyphs if latin chars and punctuation are not in font
+  const stylesheet = (url, doneCb, errCb, fontGlyphs) => {
+    const complete = (txt) => {
+      const s = document.createElement('style');
+      s.innerHTML = `/* attached by stylesheet preloader */\n\n${txt}`;
+      if (doneCb) doneCb(() => { document.head.appendChild(s); });
+    };
 
+    const fetchAssets = (txt) => {
+      let match;
+
+      const fonts = [];
+      const fontRe = /@font-face[\s\S]*?}/g;
+      const fontPropRe = /(font-)?([^: ]+?) ?: ?(.+?);/g;
+      while (match = fontRe.exec(txt)) {
+        let prop, obj = {};
+        while (prop = fontPropRe.exec(match[0]))
+          obj[prop[2].replace(/-./g, s => s[1].toUpperCase())] = prop[3];
+        obj['_original'] = match[0];
+        if (obj.family && obj.src) fonts.push(obj);
+      }
+
+      const images = [];
+      const imgRe = /url\(['"]?(.+?).(tif|jpg|jpeg|png|gif|svg|webp)['"]?\)/gi;
+      while (match = imgRe.exec(txt)) images.push(`${match[1]}.${match[2]}`);
+
+      let count = fonts.length + images.length;
+      const allCb = () => { count -= 1; if (count === 0) complete(txt); };
+      const error = () => { count = -1; if (errCb) errCb(); };
+      fonts.forEach((f) => { font(f, allCb, error, fontGlyphs); });
+      images.forEach((i) => { img(i, allCb, error); });
+    };
+
+    // fetch stylesheet data
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onload = () => { fetchAssets(xhr.responseText); };
+    xhr.onerror = () => { if (errCb) errCb(); };
+    xhr.send();
   };
 
   return {
     img: img,
     video: video,
     font: font,
-    fromStylesheet: fromStylesheet
+    stylesheet: stylesheet
   };
 })();
