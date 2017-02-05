4efb931ec74ca6c54ec70bc8d23612eb400b8923
Fabian Popa <fabpopa@users.noreply.github.com>
2017-02-05 12:32:20 +0100

    Simplify loading state

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-// expects window.site.hideAll to contain a <style> element
 // expects window.site.loading to contain a custom object
 // expects window.site.raceLoad to contain an ID returned by setTimeout
+
+// attach director, animate, and cells
+// preload stylesheet
+// when all done, clear raceLoad, hide loading, start reveal, remove hideAll
+
+
 
 window.clearTimeout(window.site.raceLoad);
 window.site.loading.hide();
