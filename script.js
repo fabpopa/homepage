3abb40766fce0d8c902b0144a3b288d8b4e89c4a
Fabian Popa <fabpopa@users.noreply.github.com>
2017-02-06 16:23:44 +0100

    Fix loading animation fade out to next step

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 // expects window.site.loading to contain a custom object
 // expects window.site.raceLoad to contain an ID returned by setTimeout
-// expects window.site.cells to contain custom object after loading cells.js
+// expects window.preload to contain a custom object after loading preload.js
+// expects window.Display to contain a constructor function after display.js
+// expects window.Director to contain a constructor function after director.js
+// expects window.site.cells to contain a custom object after loading cells.js
 
-const scriptsOk = () => {
-  // when all done, clear raceLoad, hide loading, start reveal, remove hideAll
+(() => {
+  const styleOk = (attach) => {
+    // start reveal
+    attach();
+  };
 
-  window.clearTimeout(window.site.raceLoad);
-  window.site.loading.hide();
-};
+  const scriptsOk = () => {
+    preload.stylesheet('style.css', (attach) => {
+      // clear raceLoad and hide loading
+      window.clearTimeout(window.site.raceLoad);
+      window.site.loading.hide(() => { styleOk(attach); });
+    });
+  };
 
-// attach scripts
-const scripts = ['preload', 'director', 'display', 'cells'];
-const scriptCb = (c => () => { c -= 1; if (!c) scriptsOk(); })(scripts.length);
-scripts.forEach((s) => {
-  const el = document.createElement('script');
-  el.onload = scriptCb;
-  el.src = `js/${s}.js`;
-  document.head.appendChild(el);
-});
+  // attach scripts
+  const scripts = ['preload', 'director', 'display', 'cells'];
+  const sCb = (c => () => { c -= 1; if (!c) scriptsOk(); })(scripts.length);
+  scripts.forEach((s) => {
+    const el = document.createElement('script');
+    el.onload = sCb;
+    el.src = `js/${s}.js`;
+    document.head.appendChild(el);
+  });
+})();
